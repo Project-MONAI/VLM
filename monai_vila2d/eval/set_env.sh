00d3bef2e41565c3f727f79bb3c3e9e5173a1d53
Mingxin Zheng <mingxinz@nvidia.com>
2024-10-02 02:59:29 +0000

    fix copyright in sh
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- monai_vila2d/eval/set_env.sh
+++ monai_vila2d/eval/set_env.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 export CONTAINER='/lustre/fsw/portfolios/healthcareeng/users/hroth/cache/image_vila_internal_pt2407.sqsh'
 export DATASETS='/lustre/fsw/portfolios/healthcareeng/projects/healthcareeng_monai/VLM/datasets'
 export CODE='/lustre/fsw/portfolios/healthcareeng/projects/healthcareeng_monai/VLM/code'
