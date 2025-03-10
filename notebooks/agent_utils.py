f3eb80a7e4536e2bfc9000732f77c84e8db38acf
Mingxin Zheng <mingxinz@nvidia.com>
2025-03-10 13:47:43 +0000

    fixes
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- notebooks/agent_utils.py
+++ notebooks/agent_utils.py
@@ -1,19 +1,28 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import base64
 import itertools
-import json
 import logging
-import time
 import os
 import re
 import sys
 import tempfile
+import time
 from copy import deepcopy
 from io import BytesIO
 from pathlib import Path
 from shutil import copyfile, move, rmtree
-
 from uuid import uuid4
-from zipfile import ZipFile
 
 # Third-party imports
 import nibabel as nib
